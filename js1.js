3aa859cf1b797a9bb9096362c23773c138b196af
below_v <100206243+bor1sor@users.noreply.github.com>
2022-12-02 10:44:11 +0300

    Create index1.html

---
+++ js1.js
@@ -0,0 +1,29 @@
+//alert('Hello Amigos!');
+//alert('iOS Or Android?');
+
+function f1() {
+
+    alert('Hello! You are iPhone user!');
+
+
+}
+
+function f2() {
+
+    alert('Hello! You are Xiaomi user!');
+
+
+}
+
+function f3() {
+
+    alert('Its seems like you made a mistake, click the button!');
+
+
+}
+
+function bug() {
+
+    alert('this is bug!');
+
+}
