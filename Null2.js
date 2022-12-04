a54454f5d6e2a25e44bf8909e73271108f3c41a5
Your name <you@example.com>
2022-12-04 16:42:13 +0300

    1

---
+++ Null2.js
@@ -0,0 +1,9 @@
+var cookies;
+
+while (cookies > 6) {
+  console.log("Open the box");
+  console.log("Take a cookie");
+  console.log("eat a cookie");
+  console.log("close the box");
+  cookies--;
+}
