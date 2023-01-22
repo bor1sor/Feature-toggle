657ff6078fea338eb5fa1391e105d59311b2fd13
Your name <you@example.com>
2023-01-22 23:08:14 +0300

    1

---
+++ Study1.py
@@ -0,0 +1,7 @@
+name = input ("Type your name:")
+age = input ("Type your age:")
+weight = input ("Type your weight:")
+
+print ("Hello," + name + "!")
+print ("You are" + age + "years, already")
+print ("Your weight is" + weight + "lbs")
---
+++ study2.py
@@ -0,0 +1,6 @@
+import emoji
+
+print (emoji.emojize(":cat:"))
+print (emoji.emojize(":dog:"))
+print (emoji.emojize(":rat:"))
+print (emoji.emojize(":kangaroo:"))
