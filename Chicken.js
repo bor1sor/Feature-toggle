7bf95d25e81f7b5f00b284651b85f6e9d4f6eafe
Your name <you@example.com>
2023-01-29 20:20:33 +0300

    1

--- Chicken.js
+++ Chicken.js
@@ -3,7 +3,7 @@ var chicken = 5;
 while (chicken > 0) {
   console.log("See the advertisement");
   console.log("Launch the car");
-  console.log("Go to Los Pollos Hemanos");
+  console.log("Go to Los Pollos Hermanos");
   console.log("Buy some Fried chicken");
   console.log("Done")
   chicken--;
