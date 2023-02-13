26ffc041ad6e3310821c35e45e5cc219b3fc3e89
Your name <you@example.com>
2023-02-13 23:38:21 +0300

    1

---
+++ test1.sql
@@ -0,0 +1,12 @@
+CREATE table dogs
+
+		(id INT(33) NOT NULL PRIMARY KEY,
+		sex VARCHAR (10) NULL,
+		number INT (55) NULL,
+		age INT (25) NULL,
+		color VARCHAR (10) NULL);
+        
+        INSERT INTO dogs VALUES (1, "MALE", 1, 8, "BLUE");
+        INSERT INTO dogs VALUES (2, "MALE", 2, 7, "YELLOW");
+        INSERT INTO dogs VALUES (3, "FEMALE", 3, 1, "RED");
+        INSERT INTO dogs VALUES (4, "FEMALE", 4, 4, "GREEN");
