1b8cc5a7eeaaf6cfeade6b7c6778bdeee867cc1d
Your name <you@example.com>
2024-07-20 16:54:39 +0300

    py
    py

---
+++ Python/PythonStorage/2.py
@@ -0,0 +1,9 @@
+city_temperature = int(input("enter the number:"))
+isWeather = True
+
+if isWeather:
+    print ("Foggy")
+elif city_temperature == 8:
+    print ("Rainy")
+else:
+    print ("Sunny")
